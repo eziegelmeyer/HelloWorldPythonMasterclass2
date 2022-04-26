162f81db55e3ee271897ca62f45d733df0d84bbc
eziegelmeyer <ziegelec@gmail.com>
2022-04-26 21:13:54 +0200

    removing high values in a list

---
+++ outliers.py
@@ -0,0 +1,21 @@
+data = [4, 5, 104, 105, 110, 120, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+min_valid = 100
+max_valid = 200
+
+# process the low values in the list
+stop = 0
+for index, value in enumerate(data):
+    if value >= min_valid:
+        stop = index
+        break
+
+print(stop) # for debugging
+del data[:stop]
+print(data)
+
+# process the high values in the list
+start = 0
+for index in range(len(data) - 1, -1, -1):
+    print(index)
