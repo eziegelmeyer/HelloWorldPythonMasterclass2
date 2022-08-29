c6104b05a1be7eb6175ed05c54711d2ccc8dd8d5
eziegelmeyer <ziegelec@gmail.com>
2022-08-29 21:07:38 +0200

    remove items from multiple lists then print the list

---
+++ nospam.py
@@ -0,0 +1,38 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+]
+
+
+# write code to print all the meals in the menu
+# but with spam removed
+# you can choose which approach to use
+# 1 - remove spam from each list, then print the list
+# 2 - print out the items in each list, as long as
+# it's not spam
+
+# SOLUTION THAT WORKS FOR SINGLE LIST
+# catty_list = ["egg", "bacon", "spam", "beans"]
+# snack = "spam"
+#
+# for banana in catty_list:
+#     if "spam" == banana:
+#         catty_list.remove("spam")
+#
+# print(catty_list)
+
+# 1 - remove spam from each list, then print the list
+for meal in menu:
+    for item in meal:
+        if "spam" == item:
+            meal.remove("spam")
+print(menu)
+
+# 2 - print out the items in each list, as long as
+# it's not spam
--- number_lists.py
+++ number_lists.py
@@ -2,22 +2,11 @@ empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-numbers = even + odd
+numbers = [even, odd]
 print(numbers)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-print(numbers)
+for number_list in numbers:
+    print(number_list)
 
-digits = list("43298617")
-print(digits)
-
-# more_numbers = list(numbers)
-# more_numbers = numbers[:]
-more_numbers = numbers.copy()
-print(more_numbers)
-print(numbers is more_numbers) # =! not the same list
-print(numbers == more_numbers) # equal bc they both contain the same items
-
-
-# test comment
+    for value in number_list:
+        print(value)
