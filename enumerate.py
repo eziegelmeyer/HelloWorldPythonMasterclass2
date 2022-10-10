375c6dcc275476d0ea1fa1510c21ff00232e009e
eziegelmeyer <ziegelec@gmail.com>
2022-10-10 21:38:03 +0200

    unpacking a tupple

--- enumerate.py
+++ enumerate.py
@@ -1,3 +1,10 @@
-data = [99, 88, 77, 66]
-for index, cat in enumerate(data):
-    print(index, cat)
+# for index, number in enumerate("abcdefgh"):
+#     print(index, number)
+
+for t in enumerate("abcdefgh"):
+    index, character = t
+    print(index, character)
+
+index, character = (0, 'a')
+print(index)
+print(character)
